33f42cbb3156a37d93fbe5fb421263b4c1ede34b
Matthew Dolan <mdolancode@gmail.com>
2021-12-20 23:33:37 -0800

    Add testnet for development.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,9 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 
 def deploy_simple_storage():
     # This works ONLY for local Ganache chains.
-    account = accounts[0]
+    account = get_account()
     # Brownie is smart and knows if you are going to make a transaction or call
     simple_storage = SimpleStorage.deploy({"from": account})
     stored_value = simple_storage.retrieve()
@@ -12,6 +12,13 @@ def deploy_simple_storage():
     transaction.wait(1)
     updated_store_value = simple_storage.retrieve()
     print(updated_store_value)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
