187fa5f7c0b0f670b65f3dd8706d16c179c12050
Matthew Dolan <mdolancode@gmail.com>
2021-12-20 20:49:44 -0800

    import SimpleStorage and deploy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,6 +4,7 @@ from brownie import accounts, config, SimpleStorage
 def deploy_simple_storage():
     # This works ONLY for local Ganache chains.
     account = accounts[0]
+    # Brownie is smart and knows if you are going to make a transaction or call
     simple_storage = SimpleStorage.deploy({"from": account})
     print(simple_storage)
 
